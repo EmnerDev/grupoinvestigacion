5325575975a887a011d7901317d9895272333ebc
Emner Bailon <77309667+EmnerDev@users.noreply.github.com>
2023-08-03 18:19:06 -0500

    dangerbutton

--- resources/js/Components/DangerButton.vue
+++ resources/js/Components/DangerButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="rounded-md font-bold bg-red-600 px-4 py-2 text-center  text-sm text-white hover:bg-red-700"
+        class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md  text-sm text-white tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150"
     >
         <slot />
     </button>
