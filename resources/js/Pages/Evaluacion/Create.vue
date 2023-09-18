ac0d0810a915c7dfe78438b22889bfd70068c439
EmnerDev <bailonsalgado16@gmail.com>
2023-09-17 22:05:32 -0500

    cambios-evaluacion

--- resources/js/Pages/Evaluacion/Create.vue
+++ resources/js/Pages/Evaluacion/Create.vue
@@ -20,11 +20,11 @@
                                             >Rubro I: Produccion Cientìfica, tecnològica y/o industrial orientada a la linea de investigacion</th>
                                         </tr>
                                         <tr>
-                                            <th
+                                            <!-- <th
                                                 class="border-b-2 border-blue-200 bg-gray-700 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-white"
                                             >
                                             N°
-                                            </th>
+                                            </th> -->
                                             <th
                                                 class="border-b-2 border-gray-200 bg-gray-700 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-white"
                                             >
@@ -44,41 +44,41 @@
                                                 class="border-b-2 border-gray-200 bg-gray-700 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-white"
                                             >
                                             Puntaje individual
-                                            </th>  
+                                            </th>
                                             <th
                                                 class="border-b-2 border-gray-200 bg-gray-700 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-white"
                                             >
                                             Puntaje Total
-                                            </th> 
+                                            </th>
                                             <th
                                                 class="border-b-2 border-gray-200 bg-gray-700 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-white"
                                             >
                                             Accion
-                                            </th>                                                                                     
+                                            </th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <template v-for="cri,j  in criterios">
-                                            <tr v-for=" indi, i in cri.indicador" :key="indi.id">
+                                            <tr  v-for=" indi, i in cri.indicador" :key="indi.id">
                                                 <td v-if="i===0" :rowspan="cri.indicador.length"
-                                                    class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"                                                    
+                                                    class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"
                                                 >
-                                                    <p 
+                                                    <p
                                                         class="text-gray-900 whitespace-no-wrap"
                                                     >
                                                     {{ j+1 }}
                                                     </p>
                                                 </td>
                                                 <td v-if="i===0" :rowspan="cri.indicador.length"
-                                                    class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"                                                    
+                                                    class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"
                                                 >
                                                     <p
                                                         class="text-gray-900 whitespace-no-wrap"
                                                     >
                                                     {{ cri.name }}
                                                     </p>
-                                                </td>                                                      
-                                                <td 
+                                                </td>
+                                                <td
                                                     class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"
                                                 >
                                                     <p
@@ -87,46 +87,47 @@
                                                     {{ indi.name }}
                                                     </p>
                                                 </td>
-                                                <td 
+                                                <td
                                                     class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"
                                                 >
                                                     <p
                                                         class="text-gray-900 whitespace-no-wrap"
                                                     >
-                                                    <TextInput></TextInput>
+                                                    <TextInput v-model="newItem" @blur="addItem(indi)"></TextInput>
                                                     </p>
-                                                </td> 
-                                                <td 
+                                                </td>
+                                                <td
                                                     class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"
                                                 >
                                                     <p
                                                         class="text-gray-900 whitespace-no-wrap"
                                                     >
-                                                    
+
                                                     </p>
-                                                </td> 
+                                                </td>
                                                 <td v-if="i===0" :rowspan="cri.indicador.length"
                                                     class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm"
                                                 >
                                                     <p
                                                         class="text-gray-900 whitespace-no-wrap"
                                                     >
-                                                    
+
                                                     </p>
-                                                </td>  
-                                                <td v-if="i===0" :rowspan="cri.indicador.length"                                           
+                                                </td>
+                                                <td v-if="i===0" :rowspan="cri.indicador.length"
                                                     class="border-solid border-2 border-gray-700 bg-white px-5 py-5 text-sm">
                                                 <LinkPrimaryButton class="pl-7">
                                                     <i class="fa-solid fa-plus-circle"></i>Guardar
-                                                </LinkPrimaryButton>                                                
+                                                </LinkPrimaryButton>
                                                 <DangerButton><i class="fa-solid fa-trash"></i>Cancelar</DangerButton>
-                                            </td>                                                                                                                                   
-                                            </tr>                                        
+                                            </td>
+                                            </tr>
                                         </template>
+
                                     </tbody>
                                 </table>
                         </div>
-                    </div>                   
+                    </div>
                 </form>
             </div>
         </div>
@@ -153,42 +154,78 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import { nextTick, ref } from "vue";
 import { watch, reactive } from "vue";
 import Paginator from "@/Components/Paginator.vue";
+import { computed } from 'vue';
+import { onMounted } from 'vue';
+import { onBeforeMount } from 'vue';
 
 
-const props = defineProps({   
+
+const props = defineProps({
     criterios: Object,
 });
+const indicadores = ref([]);
+const criData = ref(props.criterios || []);
+const cantidades = ref([]);
+onBeforeMount(() => {
+  criData.value = props.criterios || [];
+});
+//console.log('probando', criterios);
 
-const form = useForm({
-    name:"",
-    space_inves:"",
-    pre_group_inv:"",
-    objective:"",
-    obj_desa_soste_l_i:"",
-    services:"",
-    laboratory:"",
-    phisical_environment:"",
-    labora_equip:"",
-    pagina:"",
-    office:"",
-    annexed:"",
-    phone:"",
-    id_area:0,
-    id_linea:0,
-    id_sublinea:0,
-    id_facultad:0,
-    id_escuela:0,
-    id_persona:0
+const addItem = (item) => {
+    if(indicadores.value.length > 0){
+        indicadores.value.forEach(element => {
+            let data = indicadores.value.find(ele => ele.id === item.id);
+            if(!data)
+            {
+                indicadores.value.push(item,1)
+            }
+        })
+        console.log('indicadores', indicadores.value);
+    } else {
+        indicadores.value.push(item,1);
+        console.log('probando', item);
+        console.log('indicadores', indicadores.value);
+    }
+
+
+}
+
+const CalcularCantidadTotalPorIndicador = (criterioIndex) => {
+    if(!cantidades.value[criterioIndex]) return 0;
+        const cantidadesPorIndex = cantidades.value[criterioIndex];
+        return cantidadesPorIndex.reduce((acc, cantidad) => acc + Number(cantidad), 0);
+
+};
+
+const inicializarCantidades = () => {
+    cantidades.value = criData.value.map((criterio) => criterio.indicador.map(() => 0));
+}
+
+const cantidaadTotalPorIndicador = computed(() => {
+    const totals = [];
+    for(let i = 0; i < cantidades.value[0].length;i++){
+        let total = 0;
+        for(let j = 0; j < cantidades.value.length; j++){
+            total += Number(cantidades.value[j][i]);
+        }
+        totals.push(total);
+    }
+    return totals;
 });
 
 
-const submit = () => {
-        form.post(route("registrar.grupo"),{
-            onSuccess: () => {ok('Registro creado Correctamente')},
-        });    
-};
-const ok = (msj) => {
-    form.reset();
-    Swal.fire({title:msj, icon:'success'});
-}
+
+onMounted( async() => {
+    inicializarCantidades();
+});
+
+
+
+// return{
+
+//     cantidades,
+//     CalcularCantidadTotalPorIndicador,
+//     inicializarCantidades,
+//     cantidaadTotalPorIndicador,
+// };
 </script>
